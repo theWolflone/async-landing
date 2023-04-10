26743b4bc765ae00240679645054d588479a9c40
Santiago Lobo <lobo09.slu@gmail.com>
2023-04-09 19:16:16 -0600

    Cambio de color en los textos de los videos

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ async function fetchData(urlApi) {
               <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
             </div>
             <div class="mt-4 flex justify-between">
-              <h3 class="text-sm text-gray-200">
+              <h3 class="text-sm text-gray-300">
                 <span aria-hidden="true" class="absolute inset-0"></span>
                 ${video.snippet.title}
               </h3>
