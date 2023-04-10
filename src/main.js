95190360e154fdedd4673a2cfc652bced2ca52a5
Santiago Lobo <lobo09.slu@gmail.com>
2023-04-09 19:20:49 -0600

    Arreglo de los colores en los textos

--- src/main.js
+++ src/main.js
@@ -27,12 +27,12 @@ async function fetchData(urlApi) {
           video => `
       <div class="group relative">
             <div
-              class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+              class="w-full bg-gray-200 text-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
               <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
             </div>
-            <div class="mt-4 flex justify-between">
-              <h3 class="text-sm text-gray-300">
-                <span aria-hidden="true" class="absolute inset-0"></span>
+            <div class="mt-4 flex text-gray-200 justify-between text-gray-200">
+              <h3 class="text-sm text-gray-200">
+                <span aria-hidden="true" class="absolute inset-0 text-gray-200"></span>
                 ${video.snippet.title}
               </h3>
             </div>
