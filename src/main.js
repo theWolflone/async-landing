ab56dbdbe9879f0d7c0d17574584027daaf8a15d
Santiago Lobo <lobo09.slu@gmail.com>
2023-04-09 18:44:38 -0600

    Corrección de errores, llamado de API de vídeos al canal de mi artista favorito e instalación para hacer deploy en GitHub pages

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const API =
-  "https://youtube-v31.p.rapidapi.com/search?channelId=UCA3PLfFMuT963D-qGVjAFaw&part=snippet%2Cid&order=date&maxResults=9";
+  "https://youtube-v31.p.rapidapi.com/search?channelId=UCoRR6OLuIZ2-5VxtnQIaN2w&part=snippet%2Cid&order=date&maxResults=9";
 
-const content = null || document.getElementById("content");
+const content = null || document.getElementById('content');
+console.log(content);
 
 const options = {
 	method: 'GET',
@@ -27,7 +28,7 @@ async function fetchData(urlApi) {
       <div class="group relative">
             <div
               class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-              <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+              <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
             </div>
             <div class="mt-4 flex justify-between">
               <h3 class="text-sm text-gray-700">
